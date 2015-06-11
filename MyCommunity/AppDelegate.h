9f901f27a1af2c99bede4a4c9c3cdc76c3acaae7
Junheng <Junheng@JunhengdeMacbookPro.local>
2015-06-11 11:26:37 +0800

    Initial Commit

---
+++ MyCommunity/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyCommunity
+//
+//  Created by Junheng on 15/6/11.
+//  Copyright (c) 2015年 Junheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyCommunity/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyCommunity
+//
+//  Created by Junheng on 15/6/11.
+//  Copyright (c) 2015年 Junheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
